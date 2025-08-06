fe5ddd605b824ed88c9bf2de1c98a45178314b1c
pollettf <pollettf@gmail.com>
2025-08-06 16:12:37 -0700

    Add files via upload
    script for scholi bot

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def setup_bedrock():
         'bedrock-agent-runtime',
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-        aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
         region_name=os.getenv('AWS_DEFAULT_REGION')
     )
 
